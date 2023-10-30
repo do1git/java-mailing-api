4d6161c4f272fdb8b1b83c1e53dc456db49bad55
do1git <dev.dothe1@gmail.com>
2023-10-30 21:53:25 +0900

    [NEW] smtpClient base

--- src/main/java/com/example/javamailingapi/domain/api/MainController.java
+++ src/main/java/com/example/javamailingapi/domain/api/MainController.java
@@ -1,9 +1,14 @@
 package com.example.javamailingapi.domain.api;
 
 import com.example.javamailingapi.domain.model.SendRequest;
+import com.example.javamailingapi.domain.model.SendResponse;
 import com.example.javamailingapi.domain.service.MailService;
 import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -18,10 +23,17 @@ public class MainController {
 
   private final MailService mailService;
 
-  @PostMapping("/send")
   @ApiOperation(value = "메일전송", notes = "smtp로 전송")
-  public ResponseEntity<?> sendRequest(@RequestBody SendRequest signupDto, Errors errors){
-    return null;
+  @ApiResponses(value = {@ApiResponse(code = 200, message = "OK", response = SendResponse.class)})
+  @PostMapping(
+      value = "/send",
+      consumes = MediaType.APPLICATION_JSON_VALUE,
+      produces = MediaType.APPLICATION_JSON_VALUE)
+  public SendResponse sendRequest(
+      @ApiParam(value = "상품 연동(최초 연동) / 재연동(이미 연동한 경우)", required = true) @RequestBody SendRequest sendRequest) {
+    SendResponse sendResponse = mailService.sendEmail(sendRequest);
+
+    return sendResponse;
   }
 
 
