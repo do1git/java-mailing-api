22adc12ae33ec9b037134c15d3963f1ff250dd5a
Hyeongjin <lipfolnp@gmail.com>
2023-10-31 18:01:19 +0900

    [ADD]file attachment email sending

--- src/main/java/com/example/javamailingapi/domain/api/MainController.java
+++ src/main/java/com/example/javamailingapi/domain/api/MainController.java
@@ -15,26 +15,53 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("/api")
 public class MainController {
 
-  private final MailService mailService;
+    private final MailService mailService;
 
-  @ApiOperation(value = "메일전송", notes = "smtp로 전송")
-  @ApiResponses(value = {@ApiResponse(code = 200, message = "OK", response = SendResponse.class)})
-  @PostMapping(
-      value = "/send",
-      consumes = MediaType.APPLICATION_JSON_VALUE,
-      produces = MediaType.APPLICATION_JSON_VALUE)
-  public SendResponse sendRequest(
-      @ApiParam(value = "상품 연동(최초 연동) / 재연동(이미 연동한 경우)", required = true) @RequestBody SendRequest sendRequest) {
-    SendResponse sendResponse = mailService.sendEmail(sendRequest);
+    @ApiOperation(value = "메일전송", notes = "smtp로 전송")
+    @ApiResponses(value = {@ApiResponse(code = 200, message = "OK", response = SendResponse.class)})
+    @PostMapping(
+            value = "/send",
+            consumes = MediaType.MULTIPART_FORM_DATA_VALUE,
+            produces = MediaType.APPLICATION_JSON_VALUE)
+    public SendResponse sendRequest(
+            @ApiParam(value = "송신 이메일", required = true)
+            @RequestParam("1. senderEmail") String senderEmail,
 
-    return sendResponse;
-  }
+            @ApiParam(value = "비밀번호", required = true)
+            @RequestParam("2. password") String password,
 
+            @ApiParam(value = "수신 이메일", required = true)
+            @RequestParam("3. receiverEmail") String receiverEmail,
 
+            @ApiParam(value = "제목", required = true)
+            @RequestParam("4. title") String title,
+
+            @ApiParam(value = "본문", required = true)
+            @RequestParam("5. content") String content,
+
+            @ApiParam(value = "첨부파일", required = false)
+            @RequestParam(value = "6. attachment", required = false) MultipartFile attachment) {
+
+        SendRequest sendRequest = new SendRequest();
+        sendRequest.setSenderEmail(senderEmail);
+        sendRequest.setPassword(password);
+        sendRequest.setReceiverEmail(receiverEmail);
+        sendRequest.setTitle(title);
+        sendRequest.setContent(content);
+        if (attachment != null && !attachment.isEmpty()) {
+            sendRequest.setAttachment(attachment);
+        }
+
+        SendResponse sendResponse = mailService.sendEmail(sendRequest);
+
+        return sendResponse;
+    }
 }
--- src/main/java/com/example/javamailingapi/domain/model/SendRequest.java
+++ src/main/java/com/example/javamailingapi/domain/model/SendRequest.java
@@ -7,21 +7,17 @@ import org.springframework.web.multipart.MultipartFile;
 
 @Data
 public class SendRequest {
-
   @ApiModelProperty(notes = "송신 이메일", position = 1, example = "@naver.com")
   private String senderEmail;
   @ApiModelProperty(notes = "비밀번호", position = 2, example ="''")
   private String password;
   @ApiModelProperty(notes = "수신 이메일", position = 3, example ="''")
   private String receiverEmail;
-  @ApiModelProperty(notes = "참조", position = 4)
-  private List<String> ccList;
-  @ApiModelProperty(notes = "숨김참조", position = 5)
-  private List<String> bccList;
-  @ApiModelProperty(notes = "제목", position = 6, example ="''")
+   @ApiModelProperty(notes = "제목", position = 4, example ="''")
   private String title;
-  @ApiModelProperty(notes = "본문", position = 7, example ="''")
+  @ApiModelProperty(notes = "본문", position = 5, example ="''")
   private String content;
-  @ApiModelProperty(notes = "파일", position = 8)
-  private List<MultipartFile> files;
+  @ApiModelProperty(notes = "파일", position = 6, example ="''")
+  private MultipartFile attachment;
+
 }
